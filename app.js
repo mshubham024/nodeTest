f61a703fce9b96d6adffbb653dd01bac6cca5af1
Shubham Mishra <shubham@bbd.co.za>
2024-05-31 11:55:03 +0530

    added-new-endpoint

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get('/', (req,res) => {
     res.send('hello world this is for test')
 });
 
+app.get('/test', (req,res) => {
+    res.send('Hi your test is success')
+});
+
 app.listen(3000, () => console.log('port is up and running'));
